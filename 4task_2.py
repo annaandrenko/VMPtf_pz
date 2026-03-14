5793203e31bfb2193e870c80f51d357e181a66e5
Anya A <anna.andrenko@nure.ua>
2026-03-14 15:12:48 +0200

    2 варіант, 3,4 завдання

--- 4task_2.py
+++ 4task_2.py
@@ -1,6 +1,7 @@
 #варіант 2
 import requests
 
+
 url = "https://api.github.com" # URL публічного API
 response = requests.get(url) # відправляємо запит до API
 data = response.json() # отр дані у форматі JSON
